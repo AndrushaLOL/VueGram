e7bfb7a90d01f3fd0841089d1686d71020a15b3c
AndrushaLOL <takemyenergy55@gmail.com>
2018-08-07 10:34:28 +0500

    initial project

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title>VueGram</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-title>{{userName}}</v-toolbar-title>
+        <v-toolbar-items class="hidden-sm-and-down ml-4" >
+            <v-btn flat><v-avatar
+                size="48px"
+                color="red"
+            >
+                <img :src="profileImg" alt="alt">
+            </v-avatar></v-btn>
+        </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {
+    name: "app-toolbar",
+    props: {
+        userName: String,
+        profileImg: String
+    }
+}
+</script>
+
---
+++ src/components/Post.vue
@@ -0,0 +1,43 @@
+<template>
+<v-card elevation-3>
+    <v-toolbar color>
+        <v-toolbar-title>
+            <v-avatar
+                color="red"
+            >
+                <img :src="profileImg" alt="alt">
+            </v-avatar>
+            <span>{{userName}}</span>
+            
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-card-media
+        
+        height="400px"
+        :src="image"
+    >
+        
+    </v-card-media>
+    <v-card-text>
+        <span class="black--text">{{userName}}:</span>
+        <span class="grey--text">{{postDescr}}</span>
+    </v-card-text>
+</v-card>
+</template>
+
+<script>
+export default {
+    name: 'Post',
+    data () {
+        return {}
+    },
+    props: {
+        profileImg: String,
+        userName: String,
+        postDescr: String,
+        likes: Number,
+        image: String
+    }
+}
+</script>
